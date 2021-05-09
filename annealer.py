039eb9845eddfbb347571d278dfb4cf36fb6ab04
Lizaterdag <52582827+Lizaterdag@users.noreply.github.com>
2021-05-09 16:44:17 +0200

    Update annealer.py

--- annealer.py
+++ annealer.py
@@ -9,16 +9,12 @@ import matplotlib
 matplotlib.use("agg")
 from matplotlib import pyplot as plt
 
-
+sampler = EmbeddingComposite(DWaveSampler())
 
 G = nx.Graph()
 G.add_edges_from([(1,2), (1,3), (2,3), (3,4), (3,5), (4,5), (4,6), (5,6), (6,7)])
 
 Q = min_vertex_color_qubo(G)
-quench_schedule=[[0.0, 0.0], [12.0, 0.6], [12.8, 1.0]]
-#DWaveSampler().validate_anneal_schedule(quench_schedule) 
-
-sampler = EmbeddingComposite(DWaveSampler)
 
 sampleset = sampler.sample_qubo(Q, num_reads = 100)
 for sample, energy, num_occ in sampleset.data(['sample', 'energy', 'num_occurrences']):
